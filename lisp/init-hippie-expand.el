f8b0de252de16548263a66f107dddfe812b1da88
Che <cheezy64@gmail.com>
2014-10-27 02:47:36 -0500

    Attempts to support ctags with hippie expand only if tags file is already present

--- lisp/init-hippie-expand.el
+++ lisp/init-hippie-expand.el
@@ -1,3 +1,7 @@
+;; ;TODO: Implement a completion list with company.  Can leverage code here
+;;   http://stackoverflow.com/questions/6515009/how-to-configure-emacs-to-have-it-complete-the-path-automatically-like-vim/6556788#6556788
+;; ;TODO: Make "tags-completion-table" default to TAGS file
+;; ;TODO: Traverse up directory for TAGS file
 (global-set-key (kbd "M-/") 'hippie-expand)
 
 ;; let hippie-expand support ctags
@@ -20,53 +24,55 @@
 
 ;; The actual expansion function
 (defun try-expand-tag (old)
-  ;; old is true if we have already attempted an expansion
-  (cond
-   ((memq major-mode '(org-mode
-                       message-mode))
-    (let ((lookup-func (if (fboundp 'ispell-lookup-words)
-                           'ispell-lookup-words
-                         'lookup-words)))
-      (unless old
-        (he-init-string (he-lisp-symbol-beg) (point))
-        (if (not (he-string-member he-search-string he-tried-table))
-            (setq he-tried-table (cons he-search-string he-tried-table)))
-        (setq he-expand-list
-              (and (not (equal he-search-string ""))
-                   (funcall lookup-func (concat (buffer-substring-no-properties (he-lisp-symbol-beg) (point)) "*")))))
-      (if (null he-expand-list)
-          (progn
-            (if old (he-reset-string))
-            ())
-        (progn
-          (he-substitute-string (car he-expand-list))
-          (setq he-expand-list (cdr he-expand-list))
-          t))
-      ))
-   ((string= major-mode "minibuffer-inactive-mode") ())
-   (t
-    (unless old
-      ;; he-init-string is used to capture the string we are trying to complete
-      (he-init-string (he-tag-beg) (point))
-      ;; he-expand list is the list of possible expansions
-      (setq he-expand-list (sort
-                            (all-completions he-search-string 'tags-complete-tag) 'string-lessp)))
-    ;; now we go through the list, looking for an expansion that isn't in the table of previously
-    ;; tried expansions
-    (while (and he-expand-list
-                (he-string-member (car he-expand-list) he-tried-table))
-      (setq he-expand-list (cdr he-expand-list)))
-    ;; if we didn't have any expansions left, reset the expansion list
-    (if (null he-expand-list)
-        (progn
-          (when old (he-reset-string))
-          ())
-      ;; otherwise offer the expansion at the head of the list
-      (he-substitute-string (car he-expand-list))
-      ;; and put that expansion into the tried expansions list
-      (setq he-expand-list (cdr he-expand-list))
-      t)
-    )))
+  (let ((tag-file (concat default-directory "TAGS")))
+    (if (file-exists-p tag-file)
+        ;; old is true if we have already attempted an expansion
+        (cond
+         ((memq major-mode '(org-mode
+                             message-mode))
+          (let ((lookup-func (if (fboundp 'ispell-lookup-words)
+                                 'ispell-lookup-words
+                               'lookup-words)))
+            (unless old
+              (he-init-string (he-lisp-symbol-beg) (point))
+              (if (not (he-string-member he-search-string he-tried-table))
+                  (setq he-tried-table (cons he-search-string he-tried-table)))
+              (setq he-expand-list
+                    (and (not (equal he-search-string ""))
+                         (funcall lookup-func (concat (buffer-substring-no-properties (he-lisp-symbol-beg) (point)) "*")))))
+            (if (null he-expand-list)
+                (progn
+                  (if old (he-reset-string))
+                  ())
+              (progn
+                (he-substitute-string (car he-expand-list))
+                (setq he-expand-list (cdr he-expand-list))
+                t))
+            ))
+         ((string= major-mode "minibuffer-inactive-mode") ())
+         (t
+          (unless old
+            ;; he-init-string is used to capture the string we are trying to complete
+            (he-init-string (he-tag-beg) (point))
+            ;; he-expand list is the list of possible expansions
+            (setq he-expand-list (sort
+                                  (all-completions he-search-string 'tags-complete-tag) 'string-lessp)))
+          ;; now we go through the list, looking for an expansion that isn't in the table of previously
+          ;; tried expansions
+          (while (and he-expand-list
+                      (he-string-member (car he-expand-list) he-tried-table))
+            (setq he-expand-list (cdr he-expand-list)))
+          ;; if we didn't have any expansions left, reset the expansion list
+          (if (null he-expand-list)
+              (progn
+                (when old (he-reset-string))
+                ())
+            ;; otherwise offer the expansion at the head of the list
+            (he-substitute-string (car he-expand-list))
+            ;; and put that expansion into the tried expansions list
+            (setq he-expand-list (cdr he-expand-list))
+            t)
+          )))))
 
 ;; Done, now we just use it as a clause in our make-hippie-expand-function (as above)
 (setq hippie-expand-try-functions-list
@@ -75,6 +81,8 @@
         try-expand-dabbrev
         try-expand-dabbrev-all-buffers
         try-expand-dabbrev-from-kill
-        try-expand-tag))
+        try-expand-tag
+        )
+      )
 
 (provide 'init-hippie-expand)
